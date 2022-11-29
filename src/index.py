a6d56954106d79b9f7d9b515427ac60e36155e22
Joonatan <j.joonatan.mustonen@gmail.com>
2022-11-29 15:52:26 +0200

    Updated game

--- src/index.py
+++ src/index.py
@@ -1,151 +1,111 @@
 from __future__ import annotations
-import pygame, sys
+import sys
+import pygame
 import numpy as np
 
+from logic import create_arrows
+from logic import Tile
 
 RED = 255, 0, 0
 GREEN = 0, 255, 0
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 
-RADIUS = 60
-
-VECTORS = np.array([
-    [1, 0],
-    [np.cos(np.radians(120)), np.sin(np.radians(120))],
-    [np.cos(np.radians(240)), np.sin(np.radians(240))]
-])
+HEX_DIST_FROM_CENTER = 30
+ARROW_SIZE = 70
 
 
-def rotation_matrix(theta):
-    theta_radians = np.radians(theta)
-    return np.array([
-        [np.cos(theta_radians), -np.sin(theta_radians)],
-        [np.sin(theta_radians), np.cos(theta_radians)]
-    ])
+WIDTH = 2*640
+HEIGHT = 2*480
 
-class Hex:
-    def __init__(self, radius, center):
-        self.radius = radius
-        self.dist_center = np.array([0.0, 0.0])
-
-        self.points = []
-        theta = np.radians(60)
-        for i in range(6):
-            x_pos = radius*np.cos(i*theta + np.radians(30)) + radius*center[0]
-            y_pos = radius*np.sin(i*theta + np.radians(30)) + radius*center[1]
-            self.points.append([x_pos, y_pos])
-        self.points = np.array(self.points)
-
-    def move(self, vector):
-        if isinstance(vector, list):
-            vector = np.array(vector)
-        self.dist_center += vector*self.radius
-        self.points += self.radius*vector
-    
-    def draw(self,screen, remove):
-        if remove:
-            pygame.draw.polygon(screen, BLACK, self.points + [640, 480])
-            pygame.draw.polygon(screen, BLACK, self.points + [640, 480], 3)
-        else:
-            pygame.draw.polygon(screen, GREEN, self.points + [640, 480])
-            pygame.draw.polygon(screen, RED, self.points + [640, 480], 3)
+CENTER_POS = [WIDTH//2, HEIGHT//2]
 
 
-class Tile:
-    def __init__(self, radius):
-        vectors = np.array([
-            [np.cos(np.radians(30)), 0.0],
-            [-np.cos(np.radians(30)), 0.0],
-            [0.0, 3/2],
-            [0.0, -3/2]
-        ])
+def draw_polygon(screen, color, line_color, points):
+    pygame.draw.polygon(screen, color, points + CENTER_POS)
+    pygame.draw.polygon(screen, line_color, points + CENTER_POS, 2)
 
-        self.hexes = [
-            Hex(radius, vector) for vector in vectors
-        ]
 
-    def move(self, vector):
-        hex:Hex
-        for hex in self.hexes:
-            hex.move(vector)
-    
-    def rotate(self, theta):
-        r = rotation_matrix(theta)
-        hex:Hex
-        for hex in self.hexes:
-            for i, point in enumerate(hex.points):
-                pos = point - hex.dist_center
-                hex.points[i] = r @ (point - hex.dist_center) + hex.dist_center
-    
-    def draw(self, screen, remove):
-        hex:Hex
-        for hex in self.hexes:
-            hex.draw(screen, remove)
-(0, 0, 0), ((0, 100), (0, 200), (200, 200), (200, 300), (300, 150), (200, 0), (200, 100))
+GRID = np.zeros((20, 20, 20))
 
-class Arrow:
-    def __init__(self):
-        self.poits = np.array([
-            [0, 20.0],
-            [0, 40.0],
-            [40.0, 40.0],
-            [40.0, 60.0],
-            [60.0, 30.],
-            [40.0, 0.0],
-            [40.0, 20.0]
-        ])
 
-    def rotate(self, theta):
-        r = rotation_matrix(theta)
-        for i, point in enumerate(self.poits):
-            self.poits[i] = r @ point
-    
-    def draw(self, screen):
-        pygame.draw.polygon(screen, RED, self.poits + [640, 240])
-        
+def draw_grid(screen):
+    points = []
+    theta = np.radians(60)
+    for i in range(6):
+        x_pos = HEX_DIST_FROM_CENTER*np.cos(i*theta + np.radians(30))
+        y_pos = HEX_DIST_FROM_CENTER*np.sin(i*theta + np.radians(30))
+        points.append([x_pos, y_pos])
 
+    points = np.array(points)
+
+    for i in range(-10, 10):
+        for j in range(-10, 10):
+            for k in range(-10, 10):
+                x_center = 2*(HEX_DIST_FROM_CENTER - 4) * \
+                    (i + j*np.cos(theta) - k*np.cos(theta)) + WIDTH//2
+                y_center = 2*(HEX_DIST_FROM_CENTER - 4) * \
+                    (j*np.sin(theta) + k*np.sin(theta)) + HEIGHT//2
+
+                pygame.draw.polygon(
+                    screen, WHITE, points + [x_center, y_center])
+                pygame.draw.polygon(screen, BLACK, points +
+                                    [x_center, y_center], 2)
 
 
 def main():
     pygame.init()
 
-    size = width, height = 2*640, 2*480
-    screen = pygame.display.set_mode(size)
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
 
-    tile = Tile(RADIUS)
-    arrow = Arrow()
+    draw_grid(screen)
 
+    tile = Tile(HEX_DIST_FROM_CENTER)
+
+    arrows = create_arrows(0, ARROW_SIZE)
+    for arrow in arrows:
+        arrow.move([-400, 280])
 
     while True:
 
-        tile.draw(screen, True)
-        #arrow.draw(screen)
+        for hexagon in tile.hexes:
+            draw_polygon(screen, WHITE, BLACK, hexagon.points)
+
+        mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
 
         for event in pygame.event.get():
 
-            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.QUIT:
+                sys.exit()
 
             if event.type == pygame.KEYDOWN:
 
-                if event.key == pygame.K_UP:
-                    tile.move(VECTORS[0])
-
                 if event.key == pygame.K_RIGHT:
-                    tile.rotate(30)
+                    tile.rotate(60)
 
                 if event.key == pygame.K_LEFT:
-                    tile.rotate(-30)
-                
-                if event.key == pygame.K_SPACE:
-                    tile.draw(screen, False)
-                    tile = Tile(RADIUS)
+                    tile.rotate(-60)
 
-        tile.draw(screen, False)
+                if event.key == pygame.K_SPACE:
+                    pass
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for arrow in arrows:
+                    arrow.click([mouse_pos_x - CENTER_POS[0], mouse_pos_y - CENTER_POS[1]], tile)
+
+        for hexagon in tile.hexes:
+            draw_polygon(screen, GREEN, RED, hexagon.points)
+
+        for arrow in arrows:
+            if arrow.point_in_polygon([mouse_pos_x - CENTER_POS[0], mouse_pos_y - CENTER_POS[1]]):
+                draw_polygon(screen, GREEN, RED, arrow.points)
+            else:
+                draw_polygon(screen, RED, GREEN, arrow.points)
 
         pygame.display.update()
-        clock.tick(30)
+        clock.tick(60)
+
 
 if __name__ == "__main__":
     main()
